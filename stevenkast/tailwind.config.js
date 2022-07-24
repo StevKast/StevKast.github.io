db37507d6f6867368397d6c6d878abb765a43827
Steven Kast <steven.m.kast@gmail.com>
2022-07-23 23:06:59 -0400

    Updating navbar to daisyUI

--- stevenkast/tailwind.config.js
+++ stevenkast/tailwind.config.js
@@ -4,7 +4,31 @@ module.exports = {
     './src/**/*.{html,ts}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'projects': '#6002ee',
+        'projects-text': '#cfbaff',
+        'printer': '#71F1A2',
+        'resume': '#4DD0E1',
+        'server': '#F4FF81',
+      }
+    },
+  },
+  daisyui: {
+    themes: [{
+      mytheme: {
+        "primary": "#6419E6",
+        "secondary": "#D926A9",
+        "accent": "#1FB2A6",
+        "neutral": "#4a4a55",
+        "base-100": "#282832",
+        "info": "#3ABFF8",
+        "success": "#36D399",
+        "warning": "#FBBD23",
+        "error": "#F87272",
+      },
+    }
+    ],
   },
   plugins: [require("daisyui")],
 }
